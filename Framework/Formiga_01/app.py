b0051698b6a9a6aa285aeccfe597051ababafb7f
FelipeKatao <cataofelipe@gmail.com>
2019-03-28 01:38:54 -0300

    Back end project install

--- Framework/Formiga_01/app.py
+++ Framework/Formiga_01/app.py
@@ -1,14 +1,21 @@
-from flask import Flask, url_for
+from flask import Flask, url_for,request
+from flask import render_template
+from os import listdir
+
 app = Flask(__name__)
 
 @app.route('/')
 def api_root():
-    return 'Welcome'
+    return render_template("test.html")
 
-@app.route('/articles')
+@app.route('/Oat',methods=['GET','POST'])
 def api_articles():
-    return 'List of ' + url_for('api_articles')
-
+    if request.method == 'POST':
+        with open('Data_01/test3.txt', 'w+') as f:
+            f.write(str("Internal source data"))
+        return "Atualizado"
+    return f
+                                    
 @app.route('/articles/<articleid>')
 def api_article(articleid):
     return 'You are reading ' + articleid
